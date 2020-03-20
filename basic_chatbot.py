c902a5c11926ee111454b8b6036de6f5910bac90
Berker <berkererdini@gmail.com>
2020-03-20 04:58:51 -0500

    First commit

---
+++ basic_chatbot.py
@@ -0,0 +1,7 @@
+print ("Welcome to my first chatbot")
+
+print ("This chatbot is designed to repeat whatever its owner says")
+
+stuff_to_echo = input("What would you want your chatbot to say?")
+
+print("You said: ", stuff_to_echo)
